c78ede6bd0b13cd23d9202f167a77aac10b62894
amankr8 <amankumar141099@gmail.com>
2025-01-27 10:07:45 +0530

    Code refactoring

---
+++ src/main/resources/db/migration/V002_Insert_root_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.mercheazy_user
+(u_create_date, u_id, u_update_date, u_email, u_password, u_role, u_username)
+VALUES(CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 'hello@mercheazy.com', 'admin', 'ADMIN', 'mercheazy');
