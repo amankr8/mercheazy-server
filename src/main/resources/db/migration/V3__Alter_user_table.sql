07f7233ea6666f50195106eefd2c8190c4e4a80e
amankr8 <amankumar141099@gmail.com>
2025-03-01 15:45:10 +0530

    Code refactor

--- src/main/resources/db/migration/V3__Alter_user_table.sql
+++ src/main/resources/db/migration/V3__Alter_user_table.sql
@@ -1,9 +1,9 @@
 --Add columns to the users table
-ALTER TABLE mercheazy_user
+ALTER TABLE merch_user
 ADD COLUMN u_first_name VARCHAR(100) NOT NULL DEFAULT 'User',
 ADD COLUMN u_last_name VARCHAR(100);
 
-UPDATE mercheazy_user
+UPDATE merch_user
 SET u_first_name = 'MerchEazy',
     u_last_name = 'Admin'
 WHERE u_username = 'mercheazy';
