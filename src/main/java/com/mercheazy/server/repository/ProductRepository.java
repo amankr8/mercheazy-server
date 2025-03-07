7e3d290cf1c8fd39c07c09f4c191b763c622beed
amankr8 <amankumar141099@gmail.com>
2025-03-07 21:13:34 +0530

    Product controllers updated

--- src/main/java/com/mercheazy/server/repository/ProductRepository.java
+++ src/main/java/com/mercheazy/server/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.mercheazy.server.repository;
 
 import com.mercheazy.server.entity.Product;
+import com.mercheazy.server.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findByStore(Store store);
 }
