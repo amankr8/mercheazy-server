90991a7416795574c041542798f1cd5d7fcd3ac0
amankr8 <amankumar141099@gmail.com>
2025-03-12 12:55:54 +0530

    Code refactor

--- src/main/java/com/mercheazy/server/repository/ProductImageRepository.java
+++ src/main/java/com/mercheazy/server/repository/ProductImageRepository.java
@@ -1,11 +1,10 @@
 package com.mercheazy.server.repository;
 
-import com.mercheazy.server.entity.Product;
 import com.mercheazy.server.entity.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Integer> {
-    List<ProductImage> findByProduct(Product product);
+    List<ProductImage> findByProductId(int id);
 }
