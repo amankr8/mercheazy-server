cc34c65c4595129c1b0dd9d491ab75e3c6b70757
amankr8 <amankumar141099@gmail.com>
2025-03-23 00:04:00 +0530

    OAuth2 added with Google OAuth

--- src/main/java/com/mercheazy/server/config/SecurityConfig.java
+++ src/main/java/com/mercheazy/server/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.mercheazy.server.util.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
