d84e68f0eacf93df94eab8caacb9414be0b72b03
amankr8 <amankumar141099@gmail.com>
2025-03-23 21:00:22 +0530

    OAuth2 added with Google OAuth

--- src/main/java/com/mercheazy/server/config/AppConfig.java
+++ src/main/java/com/mercheazy/server/config/AppConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @RequiredArgsConstructor
 @Configuration
@@ -21,6 +22,11 @@ public class AppConfig {
     UserDetailsService userDetailsService() {
         return username -> userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     @Bean
